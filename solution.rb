9b17b68d4fe997d7cc6b9f6f635443a1834db0c5
Alex Liu <yuningalexliu@gmail.com>
2016-08-27 13:25:58 -0400

    finished p11

---
+++ solution.rb
@@ -0,0 +1,34 @@
+i = 1
+
+def calculate_ith_triangle_number(i)
+    answer = 0
+    while i>0
+        answer += i
+        i = i-1 
+    end
+    return answer
+end
+
+def calculate_number_of_divisors(n)
+    number_of_divisors = 0
+    i = 1
+    while i <= n
+        if n%i == 0
+            number_of_divisors += 1
+        end
+        i += 1
+    end
+    return number_of_divisors
+end
+
+while true
+    triangle_number = calculate_ith_triangle_number(i)
+    puts "the #{i} triangle number is #{triangle_number}, it has #{calculate_number_of_divisors(triangle_number)} divisors"
+    if calculate_number_of_divisors(triangle_number) > 500
+        puts "the first triangle number to have over 500 divisors is #{triangle_number}"
+        break
+    else
+        i += 1
+    end
+end
+
