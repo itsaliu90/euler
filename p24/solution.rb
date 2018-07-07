0f0a70e9fbb1c9f7fa534ce2c6a1e4464322b2f3
Alex Liu <yuningalexliu@gmail.com>
2018-07-07 18:53:06 -0400

    solved p24

--- p24/solution.rb
+++ p24/solution.rb
@@ -1,9 +1,4 @@
-i = 1
-sum = 0
-
-while i <= 1001 do
-    i_squared = i**2
-    sum += 4*i_squared - 6*(i-1)
-    i = i + 2
-    puts "total sum of side length #{i} is #{sum}"
+def find_permutation_index(number_array, index)
+  permutation_array = number_array.permutation.to_a
+  return permutation_array[index-1]
 end
