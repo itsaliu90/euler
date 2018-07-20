bead86340f9cca9b2b97637f8ac1ad55e39fc961
Alex Liu <yuningalexliu@gmail.com>
2018-07-20 00:34:22 -0400

    solved p35

--- p35/solution.rb
+++ p35/solution.rb
@@ -1,3 +1,37 @@
 #Goal: Find all primes under 1,000,000 that are rotatable
 
+require 'prime'
+
+def rotations(number)
+  rotations = [number]
+  length = number.to_s.length-1
+  
+  until length == 0
+    number = number.to_s.chars.rotate.join().to_i
+    rotations << number
+    length -= 1
+  end
+
+rotations
+end
+
+def check_if_circular_prime(number)
+  return if $list_of_circular_primes.key?(number)
+	return if number.to_s.include?("0")
+
+  rotations_array = rotations(number)
+  if (rotations_array.all? { |number| Prime.prime?(number)})
+    rotations_array.each { |number| $list_of_circular_primes[number] = true }
+  end
+end
+
+$list_of_circular_primes = {}
+
+i = 1
+while i < 1000000
+  check_if_circular_prime(i)
+  i += 1
+end
+
+$list_of_circular_primes.length
 
